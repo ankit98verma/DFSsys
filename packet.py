803e65613ec5f62f120ddcaed23e2e4b888d9c53
Ankit <ankitvermajnp@gmail.com>
2019-11-27 23:34:29 -0800

    made some progress. Gonna test UDP transmit

---
+++ packet.py
@@ -0,0 +1,10 @@
+
+import sys
+
+
+class DS_packet:
+
+    def __init__(self):
+        a = int(34)
+
+        print(sys.getsizeof(a))
---
+++ user.py
@@ -0,0 +1,122 @@
+from packet import DS_packet
+from strargparser import StrArgParser, CommandNotExecuted
+import sys
+import socket
+import time
+import inspect
+
+class User:
+
+    def __init__(self, path="user.config"):
+        self.params = dict()
+        f = open(path, 'r')
+
+        # list of all the configuration parameters to be present in s.config
+        rq = ['IP_ADDR', 'UPD_Receive_port', 'Listen_Conn_No', 'O_Transmit_Rate']
+        for l in f.readlines():
+            if l[0] == "#":
+                l = ((l[1:]).strip())
+                words = l.split(':')
+                self.params[words[0].strip()] = words[1].strip()
+        for r in rq:
+            if r not in self.params:
+                print("Invalid 's.config' file: '%s' parameter is missing" % r)
+                exit(0)
+
+        self.par = StrArgParser("Main command parser")
+        self.is_loop = True
+        self.input_string = '>> '
+        self.add_commands()
+
+        self.welcome_msg()
+
+        self.threads = []
+
+        self.udp_transmit_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.is_udp_transmit = True
+
+
+
+    def udp_transmit_thread(self):
+        while self.is_udp_transmit:
+            self.udp_transmit_socket.sendto(bytes("hi", "utf-8"),
+                                            (self.params['IP_ADDR'], int(self.params['UPD_Receive_port'])))
+            d = int(self.params['O_Transmit_Rate'])
+            time.sleep(d)
+            print("done")
+        self.udp_transmit_socket.close()
+
+    def add_commands(self):
+        self.par.add_command("exit", "Ends the program", function=self.cmd_exit)
+
+        # self.par.add_command("start_thread", "Starts a thread", function=self.start_thread)
+        # self.par.get_command('start_session').add_optional_arguments('-sd', '--start_date',
+        #                                                              'The start date in YYYY-MM-DD format'
+        #                                                              '. Enter "exit" to use default '
+        #                                                              'analysis duration')
+        #
+        # self.par.add_command('load_session', "Loads a previously saved session", function=self.cmd_load_session)
+        # self.par.get_command('load_session').add_compulsory_arguments('-fn', '--file_name',
+        #                                                               'The name/address of the file from '
+        #                                                               'which the session is to be loaded')
+        #
+        # self.par.add_command('start_script', "Runs the script.", function=self.cmd_start_script)
+        # self.par.get_command('start_script').add_compulsory_arguments('-fn', '--file_name',
+        #                                                               "The script file which is to be executed", )
+        # self.par.get_command('start_script').add_optional_arguments('-v', '--verbose',
+        #                                                             'Prints out the commands being executed from '
+        #                                                             'the script', param_type=None)
+
+        self.par.add_command('help', "Gives the details of all the commands of session management",
+                             function=self.cmd_help)
+
+    def cmd_exit(self, out_func=print):
+        self.is_loop = False
+        self.is_udp_transmit = False
+        out_func('Exiting')
+        exit(0)
+
+    def cmd_help(self, res, out_func=print):
+        self.par.show_help(res, out_func=out_func)
+
+    def handle_input(self):
+        while self.is_loop:
+            s = input(self.input_string).strip(' ')
+            try:
+                self.is_loop = self.exec_cmd(s)
+            except CommandNotExecuted as e:
+                print(e)
+
+    def exec_cmd(self, s):
+        (cmd, res, func, out_func) = self.par.decode_command(s)
+        if res is None:
+            return True
+        param_list = list(inspect.signature(func).parameters.keys())
+        if 'res' in param_list and 'out_func' in param_list:
+            func(res, out_func=out_func)
+        elif 'res' in param_list:
+            func(res)
+        elif 'out_func' in param_list:
+            func(out_func=out_func)
+
+        if self.par.f_tmp is not None:
+            self.par.f_tmp.close()
+            self.par.f_tmp = None
+
+        if cmd == 'exit':
+            return False
+        if cmd == 'start_script':
+            return self.is_loop
+        return True
+
+    def close_threads(self):
+        for t in self.threads:
+            t.join()
+
+    def welcome_msg(self):
+        print("Welcome!\nStarting the UDP transmit and receive thread")
+
+
+if __name__ == '__main__':
+    u = User()
+    u.handle_input()
